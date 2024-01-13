3230b108101a031d46c45238590c12d07dd34a72
Precha-chang <chainara2547@gmail.com>
2024-01-13 14:42:51 +0700

    up

--- The_bar/settings.py
+++ The_bar/settings.py
@@ -125,3 +125,6 @@ STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
