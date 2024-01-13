63f27115baffe5e4c7b8ce9c96552544e544dfea
Precha-chang <chainara2547@gmail.com>
2024-01-13 14:41:34 +0700

    up 1

--- barshop/admin.py
+++ barshop/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+    
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'desc',
+        'unit',
+    ]
+    
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+    ]
+    
+@admin.register(models.CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'cart',        
+        'product',
+        'unit',
+    ]
+    
+@admin.register(models.Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',        
+        'order_code',
+        'status',
+    ]
+@admin.register(models.OrderItem)
+class OrderItem(admin.ModelAdmin):
+    list_display = [
+        'oder',        
+        'product',
+        'unit',
+    ]
+@admin.register(models.Reserve)
+class CReserveAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',        
+        'table_id',
+        'phone',
+        'status',
+    ]
+    
+    
+    
+    
